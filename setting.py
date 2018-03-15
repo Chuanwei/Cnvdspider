93bb04c3ebd4db96e85d0b8863e5d22a68044da4
Liod <26197815+LiodAir@users.noreply.github.com>
2018-03-15 16:14:29 +0800

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,21 @@
+#encoding:utf-8
+"""
+@author:Liod
+@file:setting.py
+@time:18-1-19下午4:00
+"""
+
+def adminList():
+    """
+    @函数方法:返回发送管理员的邮箱地址
+    :return: [] type list
+    """
+    # return ["2558925187@qq.com", "463266963@qq.com"]
+    return ["admin@lioder.com"]
+
+def userlist():
+    """
+    @函数方法:返回普通用户的邮箱地址
+    :return: [] tye list
+    """
+    return ["author@qq.com"]
